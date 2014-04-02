680df1f94428fd5f4f28a02a459e84ccd188ea1d
OpenShift guest <ylang365@gmail.com>
2014-04-01 23:40:44 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SusuBeta::Application.config.secret_key_base = '6644c626a66ddf71fc6e1f3992b26fd7e31d7ef7688d6b1a114926ab0d23727fcdd105fa3c5f2a92270e16425c47eaf2ae7bbb660cc498be851922a449201a98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SusuBeta::Application.config.session_store :cookie_store, key: '_susu-beta_session'
