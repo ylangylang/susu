9160d2bf75caff5acee1ba14f3567c5083c631a4
ylangylang <ylang365@gmail.com>
2014-04-03 00:40:33 +0900

    1st

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SusuProto::Application.config.secret_key_base = '4f1ffe93af411c00e3853a30439691ace0fc47eff0b20f4041ae8001b5fc78ed42fd40b5e7fa7e73dc087177ffe0a2913299738be00aef1626917e0f0238fa62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SusuProto::Application.config.session_store :cookie_store, key: '_susu_proto_session'
